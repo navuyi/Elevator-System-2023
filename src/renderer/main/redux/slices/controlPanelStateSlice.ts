6848b750e8ca7eabca368179cd428007c3dd6149
navuyi <figlusrafal@gmail.com>
2023-05-22 16:41:05 +0200

    state management

--- src/renderer/main/redux/slices/controlPanelStateSlice.ts
+++ src/renderer/main/redux/slices/controlPanelStateSlice.ts
@@ -5,34 +5,34 @@ import { PayloadAction } from "@reduxjs/toolkit";
 
 
 interface I_CONTROL_PANEL_STATE {
+    activeElevatorID: number
     pickupFloor: number
-    numberOfPeople: number
-    destinationFloors: number[]
+    destinationFloor: number
 }
 const initialState : I_CONTROL_PANEL_STATE = {
+    activeElevatorID: 0,
     pickupFloor: 0,
-    numberOfPeople: 0,
-    destinationFloors: []
+    destinationFloor: 0
 }
 
 const controlPanelStateSlice = createSlice({
     name: "controlPanelState",
     initialState: initialState,
     reducers: {
+        setActiveElevatorID: (state, action: PayloadAction<number>) => {
+            state.activeElevatorID = action.payload
+        },
         setPickupFloor: (state, action: PayloadAction<number>) => {
             state.pickupFloor = action.payload
         },
-        setNumberOfPeople: (state, action: PayloadAction<number>) => {
-            state.numberOfPeople = action.payload
-        },
-        setDestinationFloors: (state, action: PayloadAction<number[]>) => {
-            state.destinationFloors = action.payload
+        setDestinationFloor: (state, action: PayloadAction<number>) => {
+            state.destinationFloor = action.payload
         }
     }
 })
 
 // Export actions
-export const {setPickupFloor, setNumberOfPeople, setDestinationFloors} = controlPanelStateSlice.actions
+export const {setPickupFloor, setActiveElevatorID, setDestinationFloor} = controlPanelStateSlice.actions
 
 // Export reducers
 export const controlPanelReducer = controlPanelStateSlice.reducer
--- src/renderer/main/redux/store/index.ts
+++ src/renderer/main/redux/store/index.ts
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { simulationStateReducer } from "../slices/simulationStateSlice";
 import { controlPanelReducer } from "../slices/controlPanelStateSlice";
 
-const store = configureStore({
+export const store = configureStore({
     reducer: {
         simulationState: simulationStateReducer,
         controlPanelState: controlPanelReducer
@@ -13,4 +13,4 @@ const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
-
+export default store
