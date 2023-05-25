c72378290a734f71c8d1c36bbcfe5e2978fb6384
navuyi <figlusrafal@gmail.com>
2023-05-25 15:47:52 +0200

    different directions test

--- src/hooks/tests/takePoepleIn.test.ts
+++ src/hooks/tests/takePoepleIn.test.ts
@@ -50,6 +50,18 @@ describe("Should not take the person because different floors", () => {
     expect(elevator.queue.filter(p => p.location === "lobby").length).toBe(1)
 })
 
+describe("Should not take the person because different direction", () => {
+    const elevator : I_ELEVATOR = {
+        currentFloor: 5,
+        direction: "up",
+        queue: [{ pickupFloor: 1, destinationFloor: 11, direction: "down", location: "lobby"},
+                { pickupFloor: 3, destinationFloor: 12, direction: "up", location: "elevator"}
+                ]}
+    takePeopleIn(elevator, 3)
+    expect(elevator.queue.filter(p => p.location === "elevator").length).toBe(1)
+    expect(elevator.queue.filter(p => p.location === "lobby").length).toBe(1)
+})
+
 describe("Should take all people from the floor", () => {
     const elevator : I_ELEVATOR = {
         currentFloor: 5,
